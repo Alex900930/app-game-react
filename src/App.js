658189250c3dcfe270372b96117372eca1cff6f1
Alexander Herrera <Alex>
2023-09-22 14:44:00 -0400

    feat: create new component Layout and change style background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {Suspense} from 'react'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from './components/Login'
+import Layout from "./components/Layout";
+import './App.css';
 
 const App = () => {
   return (
     <Router>
       <Suspense fallback={<p>loading...</p>}>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<div className="login-background">
+            <Login />
+          </div>} />
+          <Route path="/app" element={<Layout />} />
         </Routes>
       </Suspense>
     </Router>
